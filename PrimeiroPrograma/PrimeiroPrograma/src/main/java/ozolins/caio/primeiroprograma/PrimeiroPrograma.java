3013f0e8e276465aae6e79e7f84134f508d30bb5
Caio Ozolins Mendes <caio.mendes@alunos.unis.edu.br>
2022-04-10 11:25:51 -0300

    Created Java Project PrimeiroPrograma
    Created using NetBeans

---
+++ PrimeiroPrograma/PrimeiroPrograma/src/main/java/ozolins/caio/primeiroprograma/PrimeiroPrograma.java
@@ -0,0 +1,7 @@
+package ozolins.caio.primeiroprograma;
+//@author CaioOzolins
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        System.out.println("Olá, Mundo!");
+    }
+}
